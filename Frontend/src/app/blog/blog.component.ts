53a31ddd2f9df5900d3dca821ec09af6f3056431
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-05 15:35:01 +0200

    Update blog.

--- Frontend/src/app/blog/blog.component.ts
+++ Frontend/src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  title = 'Fountain Africa | Blog';
 
-  constructor() { }
+  constructor(private titleService: Title) {
 
+}
   ngOnInit(): void {
+    this.titleService.setTitle(this.title);
   }
 
 }
